dc716925ba24c5b4d2a85ca5b184f7be5dfee776
Emersson <emerssonmota123@gmail.com>
2021-11-11 14:58:48 -0300

    first commit

---
+++ nextauth/contexts/AuthContext.tsx
@@ -0,0 +1,96 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies, destroyCookie } from "nookies";
+import Router from "next/router";
+import { api } from "../services/api";
+
+interface User {
+  email: string;
+  permissions: string[];
+  roles: string[];
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  isAuthenticated: boolean;
+  user: User | undefined;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function signOut() {
+  destroyCookie(undefined, "nextauth.token");
+  destroyCookie(undefined, "nextauth.refreshToken");
+
+  Router.push("/");
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+
+  // se tiver vazio retorna false
+  const isAuthenticated = !!user;
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post("/sessions", {
+        email,
+        password,
+      });
+
+      const { permissions, roles, token, refreshToken } = response.data;
+
+      //cookies
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        path: "/",
+      });
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        path: "/",
+      });
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+      Router.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+
+    if (token) {
+      api
+        .get("/me")
+        .then((response) => {
+          const { email, permissions, roles } = response.data;
+          setUser({ email, permissions, roles });
+        })
+        .catch(() => {
+          signOut();
+        });
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
---
+++ nextauth/pages/index.tsx
@@ -0,0 +1,45 @@
+import type { NextPage } from "next";
+import { FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  const { isAuthenticated, signIn } = useContext(AuthContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const data = {
+      email,
+      password,
+    };
+
+    await signIn(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <input
+        placeholder="email"
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+      />
+
+      <input
+        placeholder="senha"
+        type="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.currentTarget.value)}
+      />
+
+      <button type="submit">Entrar</button>
+    </form>
+  );
+};
+
+export default Home;
